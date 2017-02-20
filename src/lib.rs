f87fa55973c81a04c5d32e4770cc8a1c0bb5cd6d
Gregor Reitzenstein <dean4devil@paranoidlabs.org>
2017-02-20 13:18:00 +0100

    Cleanup

--- src/lib.rs
+++ src/lib.rs
@@ -4,142 +4,95 @@ extern crate asnom;
 
 use std::default::Default;
 
+use nom::IResult;
+
 use asnom::common::TagClass;
 use asnom::structures::{Tag, Sequence, OctetString, ExplicitTag};
 
-named!(filter <Tag>, delimited!(
-    char!('('),
-    take_until_delim,
-    char!(')')));
+pub fn parse(input: &str) -> Result<Tag, ()> {
+    match filter(input.as_bytes()) {
+        IResult::Done(_, t) => Ok(t),
+        IResult::Error(_) | IResult::Incomplete(_) => Err(()),
+    }
+}
 
-named!(take_until_delim <Tag>, alt!(
-    do_parse!(
-        ft: take_until_s!("(") >>
-        tv: many1!(delimited!(
-            char!('('),
-            take_until_delim,
-            char!(')')
-        )) >>
-        (Tag::Sequence(Sequence {
-                class: TagClass::Context,
-                inner: tv,
-                id: match ft {
-                    b"&" => 0,
-                    b"|" => 1,
-                    b"!" => 2,
-                    _ => { println!("{:?}", ft); 0 }
-                }
-        }))
-    )
-    |
-    flat_map!(take_until_s!(")"), item)
+named!(filter <Tag>, delimited!(char!('('), content, char!(')')));
+named!(filterlist <Vec<Tag>>, many1!(filter));
+named!(content <Tag>, alt!(and | or | not | match_f));
+
+named!(and <Tag>, map!(preceded!( char!('&'), filterlist),
+    | tagv: Vec<Tag> | -> Tag {
+        Tag::Sequence(Sequence {
+            class: TagClass::Context,
+            id: 0,
+            inner: tagv,
+        })
+    }
+));
+named!(or <Tag>, map!(preceded!( char!('|'), filterlist),
+    | tagv: Vec<Tag> | -> Tag {
+        Tag::Sequence(Sequence {
+            class: TagClass::Context,
+            id: 1,
+            inner: tagv,
+        })
+    }
+));
+named!(not <Tag>, map!(preceded!( char!('!'), filter),
+    | tag: Tag | -> Tag {
+        Tag::ExplicitTag(ExplicitTag {
+            class: TagClass::Context,
+            id: 2,
+            inner: Box::new(tag),
+        })
+    }
 ));
 
-named!(filterlist <Vec<Tag> >, many1!(filter));
-
-named!(filtercomp <Tag>, alt!(and_f | or_f | not_f | item));
-
-named!(and_f <Tag>,
-    do_parse!(
-        char!('&') >>
-        out: map!(filterlist,
-            | tagv: Vec<Tag> | -> Tag {
-                Tag::Sequence(Sequence {
-                    id: 0,
-                    class: TagClass::Context,
-                    inner: tagv,
-                })
-            }
-        ) >>
-        (out)
-    )
-);
-
-named!(or_f <Tag>,
-    do_parse!(
-        char!('|') >>
-        out: map!(filterlist,
-            | tagv: Vec<Tag> | -> Tag {
-                Tag::Sequence(Sequence {
-                    id: 1,
-                    class: TagClass::Context,
-                    inner: tagv,
-                })
-            }
-        ) >>
-        (out)
-    )
-);
-
-named!(not_f <Tag>,
-    do_parse!(
-        char!('!') >>
-        out: map!(filter,
-            | tag: Tag | -> Tag {
-                Tag::ExplicitTag(ExplicitTag {
-                    class: TagClass::Context,
-                    id: 2,
-                    inner: Box::new(tag),
-                })
-            }
-        ) >>
-        (out)
-    )
-);
-
-named!(item <Tag>, alt!(present | simple));
-
-named!(simple <Tag>, do_parse!(
-    k: take_until_either!("<=~>") >>
-    id: s_match_type >>
-    v: call!(nom::rest) >>
-    (Tag::Sequence(Sequence {
-        class: TagClass::Context,
-        id: id,
-        inner: vec![
-            Tag::OctetString(OctetString {
-                inner: k.to_vec(),
-                .. Default::default()
-            }),
-            Tag::OctetString(OctetString {
-                inner: v.to_vec(),
-                .. Default::default()
-            })
-        ],
-    }))
-));
+named!(match_f <Tag>, alt!(present | simple));
 
 named!(present <Tag>, do_parse!(
-    k: take_until_s!("=*") >>
+    attr: take_till!(is_delimiter) >>
+    tag!("=*") >>
     (Tag::OctetString(OctetString {
         class: TagClass::Context,
         id: 7,
-        inner: k.to_vec()
+        inner: attr.to_vec(),
     }))
 ));
 
-named!(s_match_type <u64>, alt!(equal | approx | geq | leq));
+named!(simple <Tag>, do_parse!(
+    attr: take_till!(is_delimiter) >>
+    filtertype: filtertype >>
+    value: take_until!(")") >>
+    (Tag::Sequence(Sequence {
+        class: TagClass::Context,
+        id: filtertype,
+        inner: vec![
+               Tag::OctetString(OctetString {
+                   inner: attr.to_vec(),
+                   .. Default::default()
+               }),
+               Tag::OctetString(OctetString {
+                   inner: value.to_vec(),
+                   .. Default::default()
+               })
+        ]
+    }))
+));
+
+named!(filtertype <u64>, call!(equal));
 
 named!(equal <u64>, do_parse!(
-        tag!("=") >>
-        (3) // ID of an EqualityMatch
+    char!('=') >>
+    (3)
 ));
 
-named!(approx <u64>, do_parse!(
-        tag!("~=") >>
-        (8) // ID of an EqualityMatch
-));
-
-named!(geq <u64>, do_parse!(
-        tag!(">=") >>
-        (5) // ID of an EqualityMatch
-));
-
-named!(leq <u64>, do_parse!(
-        tag!("<=") >>
-        (6) // ID of an EqualityMatch
-));
-
+pub fn is_delimiter(chr: u8) -> bool {
+    chr == '=' as u8 ||
+    chr == '<' as u8 ||
+    chr == '>' as u8 ||
+    chr == '~' as u8
+}
 
 #[cfg(test)]
 mod tests {
@@ -147,13 +100,12 @@ mod tests {
     use super::*;
 
     use std::default::Default;
-    use nom::IResult;
     use asnom::common::TagClass;
     use asnom::structures::{Tag, OctetString, Sequence, ExplicitTag};
 
     #[test]
     fn present() {
-        let f = &b"(objectClass=*)"[..];
+        let f = "(objectClass=*)";
 
         let tag = Tag::OctetString(OctetString {
             class: TagClass::Context,
@@ -163,14 +115,12 @@ mod tests {
             ],
         });
 
-        let o: IResult<&[u8], Tag> = super::filter(f);
-        let left = Vec::new();
-        assert_eq!(o, IResult::Done(&left[..], tag));
+        assert_eq!(parse(f), Ok(tag));
     }
 
     #[test]
     fn simple() {
-        let f = &b"(cn=Babs Jensen)"[..];
+        let f = "(cn=Babs Jensen)";
 
         let tag = Tag::Sequence(Sequence {
             class: TagClass::Context,
@@ -187,46 +137,88 @@ mod tests {
             ]
         });
 
-        let o: IResult<&[u8], Tag> = super::filter(f);
-        let left = Vec::new();
-        assert_eq!(o, IResult::Done(&left[..], tag));
+        assert_eq!(parse(f), Ok(tag));
     }
 
     #[test]
     fn not() {
-        let f = &b"(!(cn=Tim Howes))"[..];
+        let f = "(!(cn=Tim Howes))";
 
-        let tag = Tag::Sequence(Sequence {
+        let tag = Tag::ExplicitTag(ExplicitTag {
             class: TagClass::Context,
             id: 2,
-            inner: vec![Tag::Sequence(Sequence {
+            inner: Box::new(Tag::Sequence(Sequence {
                 class: TagClass::Context,
                 id: 3,
                 inner: vec![
-                    Tag::OctetString(OctetString {
-                        inner: vec![0x63, 0x6e],
-                        .. Default::default()
-                    }),
-                    Tag::OctetString(OctetString {
-                        inner: vec![0x54, 0x69, 0x6d, 0x20, 0x48, 0x6f, 0x77, 0x65, 0x73],
-                        .. Default::default()
-                    })
+                   Tag::OctetString(OctetString {
+                       inner: vec![0x63, 0x6e],
+                       .. Default::default()
+                   }),
+                   Tag::OctetString(OctetString {
+                       inner: vec![0x54, 0x69, 0x6d, 0x20, 0x48, 0x6f, 0x77, 0x65, 0x73],
+                       .. Default::default()
+                   })
                 ],
-            })],
+            })),
         });
 
-        let o: IResult<&[u8], Tag> = super::filter(f);
-        let left = Vec::new();
-        assert_eq!(o, IResult::Done(&left[..], tag));
+        assert_eq!(parse(f), Ok(tag));
     }
 
     #[test]
     fn and() {
-        let f = &b"(&(a=b)(b=c)(c=d))"[..];
+        let f = "(&(a=b)(b=c)(c=d))";
 
-        let o = super::filter(f);
-        println!("{:?}", o);
+        let tag = Tag::Sequence(Sequence {
+            class: TagClass::Context,
+            id: 0,
+            inner: vec![
+                Tag::Sequence(Sequence {
+                    class: TagClass::Context,
+                    id: 3,
+                    inner: vec![
+                       Tag::OctetString(OctetString {
+                           inner: vec![0x61],
+                           .. Default::default()
+                       }),
+                       Tag::OctetString(OctetString {
+                           inner: vec![0x62],
+                            .. Default::default()
+                       })
+                    ]
+                }),
+                Tag::Sequence(Sequence {
+                    class: TagClass::Context,
+                    id: 3,
+                    inner: vec![
+                       Tag::OctetString(OctetString {
+                           inner: vec![0x62],
+                           .. Default::default()
+                       }),
+                       Tag::OctetString(OctetString {
+                           inner: vec![0x63],
+                            .. Default::default()
+                       })
+                    ]
+                }),
+                Tag::Sequence(Sequence {
+                    class: TagClass::Context,
+                    id: 3,
+                    inner: vec![
+                       Tag::OctetString(OctetString {
+                           inner: vec![0x63],
+                           .. Default::default()
+                       }),
+                       Tag::OctetString(OctetString {
+                           inner: vec![0x64],
+                            .. Default::default()
+                       })
+                    ]
+                }),
+            ]
+        });
 
-        assert!(false);
+        assert_eq!(parse(f), Ok(tag));
     }
 }
